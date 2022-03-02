be65b7a63b451e4fec87e091ea6e42a78b1eb938
Anonymous <ilvi142014@gmail.com>
2022-03-02 17:58:57 +0200

    Delete stg.py

--- stg.py
+++
@@ -1,9 +0,0 @@
-from settings import *
-
-
-def add_channel(chat_id):
-        k = markup()
-        msg = texts.add_channel
-        k.row(back('channels'))
-        send(chat_id, msg, reply_markup=k)
-        stages(chat_id, 'add_channel')
